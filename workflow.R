2a55e786339c155a3d98c4255b872fc5d1f66f7f
Shanna Rucker <srucker@limno.com>
2023-11-13 10:15:36 -0500

    Add lookup tables as .rda and excel

---
+++ workflow.R
@@ -0,0 +1,21 @@
+
+library(readxl)
+library(usethis)
+
+
+
+# Import Data -------------------------------------------------------------
+
+
+# Only needed if data in Excel gets updated
+    # dumpsite_score <- read_excel("data/lookup_tables.xlsx", sheet = "dumpsite_score")
+    # use_data(dumpsite_score)
+    #
+    # dumpsite_weight <- read_excel("data/lookup_tables.xlsx", sheet = "dumpsite_weight")
+    # use_data(dumpsite_weight)
+    #
+    # trash_score <- read_excel("data/lookup_tables.xlsx", sheet = "trash_score")
+    # use_data(trash_score)
+    #
+    # connectivity_score <- read_excel("data/lookup_tables.xlsx", sheet = "connectivity_score")
+    # use_data(connectivity_score)
