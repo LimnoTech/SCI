a2b14fc9cfbdf49059bed90241c5eb21976ae9a6
Shanna Rucker <srucker@limno.com>
2024-06-20 11:29:34 -0400

    Remove spaces in site IDs within input water quality data

--- R/process.R
+++ R/process.R
@@ -7,19 +7,19 @@ format_results <- function(df){
 
   df <- df %>%
     dplyr::mutate(
-              result_processed = Result,
-              qualifier_processed = "")
+      result_format = Result,
+      qualifier_format = "")
 
   for(i in 1:nrow(df)){
 
     for(j in 1:length(quals_orig)){
 
       # If one of the qualifier symbols are found in the Results column:
-      #   1) remove it in the results_processed field
-      #   2) add it to the qualifier_processed column
-      if(grepl(quals_orig[j], df$result_processed[i]) == TRUE){
-        df$result_processed[i] = gsub(quals_orig[j], '', df$Result[i])
-        df$qualifier_processed[i] = quals_new[j]
+      #   1) remove it in the results_format field
+      #   2) add it to the qualifier_format column
+      if(grepl(quals_orig[j], df$result_format[i]) == TRUE){
+        df$result_format[i] = gsub(quals_orig[j], '', df$Result[i])
+        df$qualifier_format[i] = quals_new[j]
       }
 
     }
@@ -27,10 +27,11 @@ format_results <- function(df){
 
 
   df <- df %>%
-    dplyr::mutate(result_processed = dplyr::case_when(result_processed == "" ~ NA,
-                                                TRUE ~ result_processed)) %>%
-    dplyr::mutate(date_processed = as.Date(Date, format = "%d %b %Y"),
-                  result_processed = as.numeric(result_processed)) # Results in warning that NAs introduced by coercion - this is intended result)
+    dplyr::mutate(result_format = dplyr::case_when(result_format == "" ~ NA,
+                                                   TRUE ~ result_format)) %>%
+    dplyr::mutate(location_format = gsub("\\s+", "", Location)) %>%
+    dplyr::mutate(date_format = as.Date(Date, format = "%d %b %Y"),
+                  result_format = as.numeric(result_format)) # Results in warning that NAs introduced by coercion - this is intended result)
 
 
   return(df)
@@ -46,24 +47,24 @@ process_wq <- function(df){
   params <- c("Conductivity", "Dissolved oxygen (DO)", "Escherichia coli", "pH", "Phosphorus, Total (as P)", "Nitrogen", "Temperature, water", "Turbidity")
 
   df_processed <- df %>%
-    dplyr::filter(Location %in% loc_id,
+    dplyr::filter(location_format %in% loc_id,
                   Analyte %in% params,
-                  qualifier_processed != "?")
+                  qualifier_format != "?")
 
   # Remove results that are NA
   df_processed <- df_processed %>%
-    tidyr::drop_na(result_processed)
+    tidyr::drop_na(result_format)
 
   # Rename and select key fields
   df_processed <- df_processed %>%
     dplyr::select(
       sample_id = `Sample ID`,
-      location_id = Location,
-      date = date_processed,
+      location_id = location_format,
+      date = date_format,
       parameter = Analyte,
       unit = Unit,
-      result = result_processed,
-      qualifier = qualifier_processed
+      result = result_format,
+      qualifier = qualifier_format
     )
 
 
